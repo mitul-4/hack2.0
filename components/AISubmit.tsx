d16aada28fc3702bb9402be67329c2e4cdc40379
MITUL <mitu04112007@gmail.com>
2025-02-15 20:11:42 +0800

    hin

--- components/AISubmit.tsx
+++ components/AISubmit.tsx
@@ -6,7 +6,7 @@ type AISubmitProps = {
 const AISubmit: React.FC<AISubmitProps> = ({AIResponse, callAI}) => {
   return (
     <div className="w-full bg-white p-6 rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300 mb-6">
-      <h2 className="text-2xl font-semibold mb-4">Generate Meal Plan!</h2>
+      <h2 className="text-2xl font-semibold mb-4">Step 4: Generate Meal Plan!</h2>
       <button
         onClick={callAI}
         className="w-full bg-[#00a36c] text-white py-2 rounded-full hover:bg-[#007f4c]"
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,11 +5,11 @@ export default function Navbar() {
   return (
     <div className="w-full text-lg font-bold h-[90px] flex flex-row justify-between items-center bg-gray-100 text-black px-6 shadow-sm">
       <div className="flex flex-row gap-6">
-        <Link href="/" className="hover:text-blue-500 transition">Feed</Link>
-        <Link href="/create" className="hover:text-blue-500 transition">Create</Link>
+        <Link href="/" className="hover:text-[#00a36c] transition">Feed</Link>
+        <Link href="/create" className="hover:text-[#00a36c] transition">Create</Link>
       </div>
 
-      <Link href="/profile" className="hover:text-blue-500 transition">
+      <Link href="/profile" className="hover:text-[#00a36c] transition">
         <UserCircle size={36} className="cursor-pointer" />
       </Link>
     </div>
